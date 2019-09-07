ed8e7a96593e30758d0b83528c48af6248d5a529
ivanl <ilukomskiy@sbdagroup.com>
2019-09-08 00:27:36 +0300

    tmp

--- runner.py
+++ runner.py
@@ -17,6 +17,7 @@ desired_angle = POSITIONS[desired_angle_index]
 
 pid = PID(1, 0.1, 0.05, setpoint=1)
 
+
 def transform(angle):
     if angle > 0:
         return angle - 90
@@ -34,9 +35,12 @@ while True:
 
     angle = transform(accel.read_angle())
     diff = - desired_angle + angle
+
+    pid.setpoint = desired_angle
     control = pid(diff)
+    print("control: {}")
+
     print("angle: {}, diff: {}".format(angle, diff))
-    print("control: {}".format(control))
     SERVO_CONTROL.x.set(diff)
     time.sleep(STEP)
     ticks = ticks + 1
