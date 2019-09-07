f59f335fbd910a7acbbab90fa67835e15476cf03
ivanl <ilukomskiy@sbdagroup.com>
2019-09-08 00:37:59 +0300

    tmp

--- runner.py
+++ runner.py
@@ -15,7 +15,7 @@ ticks = 0
 desired_angle_index = 0
 desired_angle = POSITIONS[desired_angle_index]
 
-pid = PID(1, 0.4, 0.1, setpoint=1)
+pid = PID(0.8, 0.2, 0.03, setpoint=1)
 
 
 def transform(angle):
