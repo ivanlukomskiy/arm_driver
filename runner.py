a412e5ccede145e0bda0ce54324cf8048a3b0421
ivanl <ilukomskiy@sbdagroup.com>
2019-09-08 00:11:05 +0300

    tmp

--- runner.py
+++ runner.py
@@ -6,11 +6,25 @@ import accel
 accel.init()
 SERVO_CONTROL.x.set(0)
 
-desired_angle = 90
+STEP = 0.05
+POS_PERIOD = 5
+POSITIONS = [135, 45, 90]
+
+ticks = 0
+desired_angle_index = 0
+desired_angle = POSITIONS[desired_angle_index]
 
 while True:
+    if ticks > POS_PERIOD / STEP:
+        desired_angle_index = desired_angle_index + 1
+        if desired_angle_index >= len(POSITIONS):
+            desired_angle_index = 0
+        desired_angle = POSITIONS[desired_angle_index]
+        ticks = 0
+
     angle = accel.read_angle()
     diff = - desired_angle + angle
     print("angle: {}, diff: {}".format(angle, diff))
     SERVO_CONTROL.x.set(diff)
     time.sleep(0.05)
+    ticks = ticks + 1
